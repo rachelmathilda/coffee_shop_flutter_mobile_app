cb0eb7dfc9a23a7c64f9720686faeb753b881698
rachelmathilda <rachelmathilda10@gmail.com>
2026-09-23 16:34:24 +0700

    fix: gradle and proguard-rules

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,6 +26,12 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
